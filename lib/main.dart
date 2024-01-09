2ca91359900744570d5277e0af78ed15ae2e469e
Ivan Bugaenko <bugaenko1234567890@yandex.ru>
2024-01-09 22:34:53 +0600

    df;odhfoodhkfp

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
-
 class TranslationResponse {
   final String text;
 
@@ -15,29 +14,38 @@ class TranslationResponse {
     return switch (json) {
       {
         'text': String text,
-      } => TranslationResponse(
-        text: text,
-      ),
+      } =>
+        TranslationResponse(
+          text: text,
+        ),
       _ => throw const FormatException('Failed to load album.'),
     };
   }
 }
 
-// String translate(String text, String srcLang, String trgLang) {
-//   final queryParameters = {
-//     'text': text,
-//     'src_lang': srcLang,
-//     'trg_lang': trgLang
-//   };
+Future<TranslationResponse> translate(
+    String text, String srcLang, String trgLang) async {
+  final queryParameters = {
+    'text': text,
+    'src_lang': srcLang,
+    'trg_lang': trgLang
+  };
 
-//   Future<http.Response> response;
+  const url = 'http://localhost:1308/translation/translate';
 
-//   response = http.get(Uri.parse('http://localhost:1308/translation/translate').replace(queryParameters: queryParameters));
-  
-//   var res = TranslationResponse.fromJson(jsonDecode(response.) as Map<String, dynamic>).text;
-//   print(res);
-//   return res;
-// }
+  try {
+    final response = await http.get(
+        Uri.parse(url).replace(queryParameters: queryParameters),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          'Content-Type': 'application/json',
+          'Accept': '*/*'
+        });
+    return TranslationResponse.fromJson(jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>);
+  } catch (err) {
+    throw Exception('Failed to load album');
+  }
+}
 
 void main() {
   runApp(const TranslatorApp());
@@ -69,7 +77,8 @@ class TranslatorAppPage extends StatefulWidget {
   State<TranslatorAppPage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<TranslatorAppPage> with TickerProviderStateMixin{
+class _MyHomePageState extends State<TranslatorAppPage>
+    with TickerProviderStateMixin {
   final srcTextController = TextEditingController();
   final trgTextController = TextEditingController();
 
@@ -79,14 +88,12 @@ class _MyHomePageState extends State<TranslatorAppPage> with TickerProviderState
   late String srcLang;
   late String trgLang;
 
-  late String translation;
-
-  final url = 'http://localhost:1308/translation/translate';
+  late String message = 'Переводим...';
 
   Map<String, String> langToId = {
     'Немецкий': 'de_DE',
     'Английский': 'en_XX',
-    'Испанский': 'es_XX',
+    'Хинди': 'hi_IN',
     'Французский': 'fr_XX',
     'Русский': 'ru_RU',
     'Китайский': 'zh_CN',
@@ -97,12 +104,13 @@ class _MyHomePageState extends State<TranslatorAppPage> with TickerProviderState
   @override
   void initState() {
     super.initState();
-    srcTabController = TabController(length: langToId.keys.length, vsync: this, initialIndex: 1);
-    trgTabController = TabController(length: langToId.keys.length, vsync: this, initialIndex: 4);
+    srcTabController = TabController(
+        length: langToId.keys.length, vsync: this, initialIndex: 1);
+    trgTabController = TabController(
+        length: langToId.keys.length, vsync: this, initialIndex: 4);
     langList = langList = langToId.keys.toList();
     srcLang = 'Английский';
     trgLang = 'Русский';
-    translation = '123';
   }
 
   @override
@@ -126,89 +134,84 @@ class _MyHomePageState extends State<TranslatorAppPage> with TickerProviderState
     });
   }
 
-  void _getTranslation(String text, String srcLang, String trgLang) async {
-
-    final queryParameters = {
-      'text': text,
-      'src_lang': srcLang,
-      'trg_lang': trgLang
-    };
-    
-    try {
-      final response = await http.get(Uri.parse(url).replace(queryParameters: queryParameters));
-      final res = TranslationResponse.fromJson(jsonDecode(response.body) as Map<String, dynamic>).text;
-    } catch (err) {
-      
-    }
-    
-    setState(() {
-      
-    });
+  void _getTranslation(String text, String srcLang, String trgLang) {
+    trgTextController.text = message;
+    translate(text, srcLang, trgLang).then((value) => trgTextController.text = value.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 16, bottom: 16),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Padding(
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 16, bottom: 16),
             child: Column(
               children: [
                 TabBar(
                   controller: srcTabController,
-                  tabs: [for (var name in langList) Tab(child: Text(name, overflow: TextOverflow.ellipsis,))],
+                  tabs: [
+                    for (var name in langList)
+                      Tab(
+                          child: Text(
+                        name,
+                        overflow: TextOverflow.ellipsis,
+                      ))
+                  ],
                   onTap: (int id) => _selectSrcLang(id),
                 ),
-
                 TextField(
                   controller: srcTextController,
                   autofocus: true,
                   maxLines: 3,
                   decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: srcLang
-                  ),
+                      border: const OutlineInputBorder(), hintText: srcLang),
                 ),
-
                 const SizedBox(height: 16),
-
                 TabBar(
                   controller: trgTabController,
-                  tabs: [for (var name in langList) Tab(child: Text(name, overflow: TextOverflow.ellipsis,))],
+                  tabs: [
+                    for (var name in langList)
+                      Tab(
+                          child: Text(
+                        name,
+                        overflow: TextOverflow.ellipsis,
+                      ))
+                  ],
                   onTap: (int id) => _selectTrgLang(id),
                 ),
-
                 TextField(
                   controller: trgTextController,
-                  enabled: false,
+                  readOnly: true,
                   maxLines: 3,
                   decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    hintText: translation
-                  ),
+                      border: const OutlineInputBorder(),
+                      hintText: trgLang),
                 ),
-
+                const SizedBox(height: 16),
                 ElevatedButton(
                   onPressed: () {
-                    Clipboard.setData(ClipboardData(text: translation))
+                    Clipboard.setData(ClipboardData(text: trgTextController.text))
                         .then((_) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Скопировано в буфер обмена!')));
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text('Скопировано в буфер обмена!')));
                     });
                   },
                   child: const Text('Копировать'),
                 ),
               ],
-            )
-          ),
-        ),
+            )),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
-          _getTranslation(srcTextController.text, langToId[langList[srcTabController.index]]!, langToId[langList[trgTabController.index]]!),
+          _getTranslation(
+              srcTextController.text,
+              langToId[langList[srcTabController.index]]!,
+              langToId[langList[trgTabController.index]]!),
         },
         tooltip: 'Перевести',
         child: const Icon(Icons.translate),
